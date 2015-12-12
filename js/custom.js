243a129656ca16d278cf20e9a617fa5394123036
Neetesh Kumar <kumarneetesh24@gmail.com>
2015-12-12 23:25:34 +0530

    logo

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,10 @@
 $(document).ready(function(e) {
 	$('#pre-status').fadeOut();
     $('#tt-preloader').delay(350).fadeOut('slow');
-
+    $('.mylogo1').attr("id","main-logo1");
+    $('.mylogo2').attr("id","main-logo2");
+    $('#main-logo1').attr("src","images/club_logo.png");
+    $('#main-logo2').attr("src","images/club_logo.png");
 	$('#main-menu-button, #main-menu-close').click(function(e) {
 		e.preventDefault();
 
